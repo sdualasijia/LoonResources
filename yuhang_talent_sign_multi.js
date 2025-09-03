394b3ea489399c524593a2c058f418950768b3b6
sdualasijia <64189859+sdualasijia@users.noreply.github.com>
2025-09-03 13:30:31 +0800

    Update yuhang_talent_sign_multi.js

--- yuhang_talent_sign_multi.js
+++ yuhang_talent_sign_multi.js
@@ -3,7 +3,7 @@
 # ==================================
 # 余杭人才码签到 (多账户版)
 # ==================================
-cron "10 7 * * *" script-path=https://raw.githubusercontent.com/sdualasijia/LoonResources/refs/heads/main/yuhang_talent_sign_multi.js, timeout=120, tag=余杭人才码签到
+cron "50 13 * * *" script-path=https://raw.githubusercontent.com/sdualasijia/LoonResources/refs/heads/main/yuhang_talent_sign_multi.js, timeout=120, tag=余杭人才码签到
 
 [MITM]
 hostname = open.iconntech.com, yhrcm.zzb.hzyuhang.cn
