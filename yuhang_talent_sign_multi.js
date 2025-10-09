6080935ddbcbe60bf5ff06334d6cc727b44a500e
sdualasijia <64189859+sdualasijia@users.noreply.github.com>
2025-10-10 00:08:27 +0800

    Update yuhang_talent_sign_multi.js

--- yuhang_talent_sign_multi.js
+++ yuhang_talent_sign_multi.js
@@ -14,7 +14,7 @@ hostname = open.iconntech.com, yhrcm.zzb.hzyuhang.cn
 // 每个 token 用双引号括起来，并用逗号隔开
 const oldTokens = [
     "a5a0adc219ca481b8ddfe1f6b508ce02",
-    "3b3f73863a72475abc22bd0cc8f8f53a"
+    "353238c2c6d2436f8fd949a2f9e76a0e"
 ];
 const names = [
     "liuyang",
