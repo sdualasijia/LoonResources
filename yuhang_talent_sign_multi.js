979bf7aed6deb8e726db79a736b89de54d9e2432
sdualasijia <64189859+sdualasijia@users.noreply.github.com>
2026-03-05 21:47:26 +0800

    Modify token list in yuhang_talent_sign_multi.js
    Updated token list in the configuration section.

--- yuhang_talent_sign_multi.js
+++ yuhang_talent_sign_multi.js
@@ -10,8 +10,8 @@ cron "1 0 * * *" script-path=https://raw.githubusercontent.com/sdualasijia/LoonR
 // 请在这里填入你所有需要签到的 token 列表
 // 每个 token 用双引号括起来，并用逗号隔开
 const oldTokens = [
-    "aa7dd1e760cc4957b2ec923d90396c9d",
-    "a9f506d25b40477bb13fa4782f8ddbf6"
+    "628ff4c6eff74bf38adfcfe70c594105",
+    "cf53dfc7a51e4e6594c188f2222b1ddc"
 ];
 const names = [
     "liuyang",
